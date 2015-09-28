9fe0216397439ddd39aec5fd4fefce97e3184609
Kevinpgalligan <galligankevinp@gmail.com>
2015-09-28 12:07:46 +0100

    Vowel percentage method.

--- CryptoUtils.java
+++ CryptoUtils.java
@@ -1,5 +1,22 @@
+import java.util.HashMap
+
+final String VOWELS = "aeiouAEIOU";
+
 public class CryptoUtils {
     public static void main (String[] args) {
         System.out.println("Hello, world!");
+    }
+    
+    public static float get_vowel_percentage (String message) {
+        int message_length = message.length()
+        
+        int vowel_count = 0
+        for (int i = 0; i < message_length; i++) {
+            if (VOWELS.indexOf(message.charAt(i)) >= 0) {
+                vowel_count += 1   
+            }
+        }
+        
+        return ((float)vowel_count / message_length) * 100.0
     }
 }
