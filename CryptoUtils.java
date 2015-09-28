811e63d161605d7c8601f09e3fe125ee38bbddc6
Kevinpgalligan <galligankevinp@gmail.com>
2015-09-28 13:41:53 +0100

    Change naming convention.

--- CryptoUtils.java
+++ CryptoUtils.java
@@ -7,16 +7,16 @@ public class CryptoUtils {
         System.out.println("Hello, world!");
     }
     
-    public static float get_vowel_percentage (String message) {
-        int message_length = message.length()
+    public static float getVowelPercentage (String message) {
+        int messageLength = message.length()
         
-        int vowel_count = 0
-        for (int i = 0; i < message_length; i++) {
+        int vowelCount = 0
+        for (int i = 0; i < messageLength; i++) {
             if (VOWELS.indexOf(message.charAt(i)) >= 0) {
-                vowel_count += 1   
+                vowelCount += 1   
             }
         }
         
-        return ((float)vowel_count / message_length) * 100.0
+        return ((float)vowelCount / messageLength) * 100.0
     }
 }
