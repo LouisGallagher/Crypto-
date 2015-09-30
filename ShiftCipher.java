bf6a99ee5c5d3d3cc681b51892dbbac9017c0a23
Louis Gallagher <LouisGallagher@users.noreply.github.com>
2015-09-30 15:45:25 +0100

    Created ShiftCipher.java
    Tools for attacking/ making simple shift ciphers. will probably be made obsolete by up and coming affine library as shift ciphers are a subset of affine ciphers

---
+++ ShiftCipher.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class ShiftCipher
+{
+    public static String ApplyShift(String message, int shift)
+    {
+    	StringBuilder builder = new StringBuilder();
+    	String normalisedMessage = message.toLowerCase();
+    	
+    	for(char c : normalisedMessage.toCharArray())
+    	{
+    		char delta = ((int)c < 124 && (int)c > 96 ) ? (char)(((((int)c - 97) + shift) % 26) + 97) : c;
+    		builder.append(delta);
+    	}
+
+    	return builder.toString();
+    }
+}
