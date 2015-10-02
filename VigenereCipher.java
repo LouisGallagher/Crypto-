db965be0a3431cd52a7f8ea271b2ad41b6769aa8
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-02 13:11:58 +0100

    Helper functions I forgot to transfer.

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -26,5 +26,54 @@ public class VigenereCipher {
         }
         
         return resultBuilder.toString();
+    
+    public static char encryptChar (char c, char key) {
+        int cValue = getCharValue(c);
+        int keyValue = getCharValue(key);
+        
+        int newValue = (cValue + keyValue) % 26;
+        
+        char newChar = 'a' + (char)newValue;
+        
+        return newChar;
+    }
+    
+    public static char decryptChar (char c, char key) {
+        int cValue = getCharValue(c);
+        int keyValue = getCharValue(key);
+        
+        int newValue = positiveMod(cValue - keyValue, 26);
+        
+        char newChar = 'a' + (char)newValue;
+        
+        return newChar;
+    }
+    
+    public static int getCharValue (char c) {
+        /** Maps    A,a -> 0,
+         *          B,b -> 1,
+         *          ...
+         *          Z,z -> 25
+         */
+        char base;
+        if (Character.isUpperCase(c)) {
+            base = 'A';
+        }
+        else {
+            base = 'a';
+        }
+        
+        return (int)(c % base);
+    }
+    
+    public static positiveMod (int n, int m) {
+        /** (n % m) -> positive.
+         * The % operation can give negative answers.
+         * This provides a positive answer, so -1 % 2 = 1 instead of -1.
+         * 
+         * (This should probably be in a different file).
+         */
+        int result = n % m;
+        return (n < 0 ? result + m : result);
     }
 }
