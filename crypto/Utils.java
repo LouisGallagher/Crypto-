dbea66044d5c7004de4586d699789bd3e4e05f2d
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-05 15:50:11 +0100

    Update Utils.java

--- crypto/Utils.java
+++ crypto/Utils.java
@@ -5,5 +5,21 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class Utils {
-
+    public static char getCharValue (char c) {
+        /** Maps    A,a -> 0,
+         *          B,b -> 1,
+         *          ...
+         *          Z,z -> 25
+         * Should probably be able to work with different alphabets.
+         */
+        char base;
+        if (Character.isUpperCase(c)) {
+            base = 'A';
+        }
+        else {
+            base = 'a';
+        }
+        
+        return (char)(c % base);
+    }
 }
