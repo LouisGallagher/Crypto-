bfe0d6255a25a16c59c4dd8671196d33ba063eaa
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-06 14:35:25 +0100

    Update Utils.java

--- crypto/Utils.java
+++ crypto/Utils.java
@@ -5,6 +5,12 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class Utils {
+    public static int getModularMultiplicativeInverse (int x, int n) {
+        // Returns (x^-1) mod n, having assumed that x is coprime to n.
+        int result = -1;
+        // TBD
+    }
+    
     public static int positiveMod (int x, int n) {
         int result = x % n;
         if (result < 0) {
@@ -20,6 +26,7 @@ public class Utils {
          *          ...
          *          Z,z -> 25
          * Should probably be able to work with different alphabets.
+         * Should probably accept an integer.
          */
         char base;
         if (Character.isUpperCase(c)) {
