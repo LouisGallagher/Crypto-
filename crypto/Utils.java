503b8983badd533d35418eea62d5b12c55317724
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-06 13:47:01 +0100

    Update Utils.java

--- crypto/Utils.java
+++ crypto/Utils.java
@@ -5,6 +5,15 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class Utils {
+    public static int positiveMod (int x, int n) {
+        int result = x % n;
+        if (result < 0) {
+            result += n;
+        }
+        
+        return result;
+    }
+    
     public static char getCharValue (char c) {
         /** Maps    A,a -> 0,
          *          B,b -> 1,
