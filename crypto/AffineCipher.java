61c4dd83f4d75cc904b6e437baa11d40bddbad80
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-06 13:49:06 +0100

    Basic encrypt/decrypt methods.

--- crypto/AffineCipher.java
+++ crypto/AffineCipher.java
@@ -1,7 +1,21 @@
 package crypto;
 
 public class AffineCipher {
-    public static char encryptChar (char c, int keyA, int keyB) {
-        // to be done.
+    public static int affineEncrypt (int c, int keyA, int keyB, int n) {
+        /**
+         * Parameters:
+         *      c           - numeric value <n representing a character.
+         *      keyA & keyB - the keys.
+         *      n           - the alphabet size.
+         */
+        return ((keyA * c) + keyB) % n;
+    }
+    
+    public static int affineDecrypt (int c, int keyAinverse, int keyB, int n) {
+        /**
+         * Parameters:
+         *      Same as above, but keyAinverse = (a^-1) mod n
+         */
+        return Utils.positiveMod(keyAinverse * (c - b), n);
     }
 }
