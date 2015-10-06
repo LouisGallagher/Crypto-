d1e1de94998d34bb0e1496f38e62baa5ab4631b1
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-06 14:57:15 +0100

    Update Utils.java

--- crypto/Utils.java
+++ crypto/Utils.java
@@ -1,14 +1,35 @@
 package crypto;
 
 import java.util.HashMap;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.ArrayList;
 
 public class Utils {
     public static int getModularMultiplicativeInverse (int x, int n) {
         // Returns (x^-1) mod n, having assumed that x is coprime to n.
         int result = -1;
         // TBD
+    }
+    
+    public static int[] getCoprimes (int n) {
+        // Returns all numbers <n that are coprime to it.
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(1);
+        
+        boolean[] sieve = new boolean[n];
+        for (int m = 2; m < n; m++) {
+            if (n % m == 0) {
+                for (int j = m + m; j < n; j += m) {
+                    // "true" can be taken to mean "there is a
+                    // divisor of n and this".
+                    sieve[j] = true;
+                }
+            }
+            else if (!m) {
+                result.add(m);
+            }
+        }
+        
+        return result.toArray();
     }
     
     public static int positiveMod (int x, int n) {
