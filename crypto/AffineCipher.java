0fbd88162f7094aa1afe2ab63688b5e941bcdf3a
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-13 13:56:58 +0100

    Bullshit changes.

--- crypto/AffineCipher.java
+++ crypto/AffineCipher.java
@@ -1,17 +1,41 @@
 package crypto;
 
+import java.lang.StringBuilder;
+
 public class AffineCipher {
-    public static int affineEncrypt (int c, int keyA, int keyB, int n) {
+    public static String encryptString (String plaintext, int keyA, int keyB, int n) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < plaintext.length(); i++) {
+            char cipherChar = affineEncrypt(plaintext.charAt(i));
+            result.append(cipherChar);
+        }
+        
+        return result.toString();
+    }
+    
+    public static String decryptString (String ciphertext, int keyA, int keyB, int n) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char plainChar = affineDecrypt(ciphertext.charAt(i));
+            result.append(plainChar);
+        }
+        
+        return result.toString();
+    }
+    
+    public static char affineEncrypt (char c, int keyA, int keyB, int n) {
         /**
          * Parameters:
          *      c           - numeric value <n representing a character.
          *      keyA & keyB - the keys.
          *      n           - the alphabet size.
          */
-        return ((keyA * c) + keyB) % n;
+        return (char)(((keyA * c) + keyB) % n);
     }
     
-    public static int affineDecrypt (int c, int keyAinverse, int keyB, int n) {
+    public static char affineDecrypt (char c, int keyAinverse, int keyB, int n) {
         /**
          * Parameters:
          *      Same as above, but keyAinverse = (a^-1) mod n
