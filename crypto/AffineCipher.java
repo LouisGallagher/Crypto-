272877968d05b09aa553fda8471de9b049ade84b
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-14 15:47:29 +0100

    Half done shit.

--- crypto/AffineCipher.java
+++ crypto/AffineCipher.java
@@ -3,11 +3,39 @@ package crypto;
 import java.lang.StringBuilder;
 
 public class AffineCipher {
+    
+    
+    public static char digraphEncrypt (char x, char y, int keyA, int keyB, int n) {
+        /** Params: x & y - char pair to be encrypted.
+         *          n2    - size of the alphabet, SQUARED.
+         */
+        char p = x * n + y;
+        char encrypted = (char)((keyA * p + keyB) % (n * n));
+        return encrypted;
+    }
+    
+    public static char[] digraphDecrypt (char cipherChar, int keyAinverse, int keyB, int n) {
+        /** Returns pair (x & y) in slots 0 and 1 of a character array. */
+        char p = (char)Utils.positiveMod(keyAinverse * (cipherChar - keyB), n * n);
+        char y = (char)(p % n);
+        char x = (char)((p - y) / n);
+        return new char[]{x, y};
+    }
+    
+    public static String crackAffine (String ciphertext, int n) {
+        // n - alphabet size.
+        String possiblePlaintext = "";
+        Double bestEnglishScore = Double.POSITIVE_INFINITY;
+        for (int i = 0; i  < ciphertext.length(); i++) {
+            
+        }
+    }
+    
     public static String encryptString (String plaintext, int keyA, int keyB, int n) {
         StringBuilder result = new StringBuilder();
         
         for (int i = 0; i < plaintext.length(); i++) {
-            char cipherChar = affineEncrypt(plaintext.charAt(i));
+            char cipherChar = affineEncrypt(plaintext.charAt(i), keyA, keyB, n);
             result.append(cipherChar);
         }
         
@@ -17,8 +45,9 @@ public class AffineCipher {
     public static String decryptString (String ciphertext, int keyA, int keyB, int n) {
         StringBuilder result = new StringBuilder();
         
+        int keyAinverse = Utils.getInverse(keyA);
         for (int i = 0; i < ciphertext.length(); i++) {
-            char plainChar = affineDecrypt(ciphertext.charAt(i));
+            char plainChar = affineDecrypt(ciphertext.charAt(i), keyAinverse, keyB, n);
             result.append(plainChar);
         }
         
