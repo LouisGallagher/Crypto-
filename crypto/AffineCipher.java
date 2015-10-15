5aa0d6b00f6b70be531cd85f44022a79f185c729
Kevinpgalligan <galligankevinp@gmail.com>
2015-10-15 15:41:29 +0100

    (I hope) final additions and tests.

--- crypto/AffineCipher.java
+++ crypto/AffineCipher.java
@@ -3,31 +3,76 @@ package crypto;
 import java.lang.StringBuilder;
 
 public class AffineCipher {
+    public static void main (String[] args) {
+        String test1 = "ihhwvcswfrcp"; // plaintext = "affinecipher";
+        String plaintext1 = decryptString(test1, 5, 8, 26);
+        System.out.println(plaintext1);
+        
+        String plaintext2 = crackAffine(test1);
+        System.out.println(plaintext2);
+    }
+    
     public static String crackDigraph (String ciphertext, int n) {
-        // TBD.
+        String bestMatch = "";
+        double bestEnglishDiff = Double.POSITIVE_INFINITY;
+        
+        int[] aValues = Utils.getCoprimes(n * n);
+        for (int i = 0; i < aValues.length; i++) {
+            int keyA = aValues[i];
+            
+            for (int keyB = 0; keyB < n * n; keyB++) {
+                String candidate = digraphDEcrypt(ciphertext, keyA, keyB, n);
+                double englishDiff = FrequencyAnalysis.getEnglishDifference(
+                    candidate);
+                    
+                if (englishDiff < bestEnglishDiff) {
+                    bestMatch = candidate;
+                    bestEnglishDiff = englishDiff;
+                }
+            }
+        }
+        
+        return bestEnglishDiff;
+    }
+    
+    public static String digraphDecrypt (String ciphertext, int keyA, int keyB, int n) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char cipherChar = ciphertext.charAt(i);
+            char[] charPair = digraphDecrypt(cipherChar, keyA, keyB, n);
+            result.append(charPair[0]);
+            result.append(charPair[1]);
+        }
+        
+        return result.toString();
     }
     
     public static char digraphEncrypt (char x, char y, int keyA, int keyB, int n) {
         /** Params: x & y - char pair to be encrypted.
-         *          n2    - size of the alphabet, SQUARED.
+         *          n     - alphabet size.
          */
-        char p = x * n + y;
+        char p = (char)(x * n + y);
+        
         char encrypted = (char)((keyA * p + keyB) % (n * n));
+        
         return encrypted;
     }
     
     public static char[] digraphDecrypt (char cipherChar, int keyAinverse, int keyB, int n) {
         /** Returns pair (x & y) in slots 0 and 1 of a character array. */
         char p = (char)Utils.positiveMod(keyAinverse * (cipherChar - keyB), n * n);
+        
         char y = (char)(p % n);
         char x = (char)((p - y) / n);
+        
         return new char[]{x, y};
     }
     
     public static String crackAffine (String ciphertext, int n) {
         // n - alphabet size.
         String bestPlaintext = "";
-        Double lowestEnglishDifference = Double.POSITIVE_INFINITY;
+        double lowestEnglishDifference = Double.POSITIVE_INFINITY;
         
         int[] possibleKeys = Utils.getCoprimes(n);
         for (int i = 0; i < possibleKeys.length; i++) {
@@ -35,7 +80,7 @@ public class AffineCipher {
             
             for (int keyB = 0; keyB < n; keyB++) {
                 String possiblePlaintext = decryptString(ciphertext, keyA, keyB, n);
-                Double englishDifference = FrequencyAnalysis.getEnglishDifference(
+                double englishDifference = FrequencyAnalysis.getEnglishDifference(
                     possiblePlaintext);
                 
                 if (englishDifference < lowestEnglishDifference) {
